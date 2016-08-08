8af05ae7fe6486fc771eeac26d73287f4f39b025
gusnu <gustavoadolfonunez@gmail.com>
2016-08-08 18:06:44 -0300

    qdfgh

---
+++ wp-content/themes/perfe/footer.php
@@ -0,0 +1,6 @@
+<footer>
+   <small>hola@estudioperfe.com © <?php echo date("Y") ?></small>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/perfe/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="<?php bloginfo('language'); ?>">
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo('Estudio Perfe'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
+    <?php wp_head(); ?>
+  </head>
+  <body>
+
+    <header>
+      <h1><?php bloginfo('name'); ?></h1>
+    </header>
