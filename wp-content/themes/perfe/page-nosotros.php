33409dfc53fac00d2ecf86c4d0112d43de2facb5
Gustavo Adolfo Núñez <Gustavo@iMac-de-Gabriel.local>
2016-08-22 18:40:52 -0300

    cambios

--- wp-content/themes/perfe/page-nosotros.php
+++ wp-content/themes/perfe/page-nosotros.php
@@ -7,13 +7,86 @@ Template Name: nosotros
 <?php get_header(); ?>
 
 
-<section class="container">
+<section class="container" id="quienes-1">
   <div class="row">
-    <div class="col-md-10 col-md-offset-1"><h2 class="text-center" id="parrafo-cabecera">El desarrollo de cualquier proyecto es la conclusión de un tratamiento. El proceso debe desbordar empatía con su origen y lucidez en el objetivo. </h2> </div>
+    <div class="col-md-10 col-md-offset-1 qs">
+      <img src="<?php bloginfo('url');?>/wp-content/themes/perfe/images/n_03.jpg">
+      <p>
+        Perfe es una agencia boutique de diseño gráfico, con sede en Santiago de Chile. Nos basamos en una fuerte pasión por la comunicación, el proceso de diseño y la comprensión constante del desarrollo de experiencias.
+      </p>
+
+      <p>
+        La oficina fue fundada por Camilo Huinca y Francisco Meneses en 2014. Desde el principio ha tenido como principal motivación el desarrollo de proyectos en los cuales se pueda complementar el desafio comunicativo, con la dirección creativa.
+      </p>
+
+      <p>
+        Trabajamos con empresas, organizaciones e individuos en todos los sectores culturales, públicos y privados, tanto a nivel local como internacional. Buscamos potenciar nuestra producción, y refinar los resultados; congregando especialistas oportunos para la ejecución de cada proyecto.
+      </p>
+
+      <p>
+        Somos independientes, por lo que no nos atamos a ninguna filosofía, tomamos cada trabajo individualmente y lo desarrollamos a mano. Investigamos, experimentamos y exploramos las distintas soluciones. Este enfoque nos mantiene ágiles y expectantes.
+      </p>
+
+      <p>
+        Nos especializamos en la producción y desarrollo de identidades coorporativas, marcas, editorial, visualísmo, diseño de productos y motion graphics.
+      </p>
+
+ </div>
   </div>
 </section>
-<section class="container-fluid" id="foto">
 
+<section class="container-fluid" id="camilo">
+  <div class="container">
+    <h1>Camilo</h1>
+  </div>
+</section>
+
+<section class="container-fluid" id="datos-camilo">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 redes text-right">
+        <h4>Instagram</h4>
+        <a href="http://www.Instagram.com/onlyjoke">@onlyjoke</a>
+        <br />  <br />
+        <h4>Twitter</h4>
+        <a href="http://twitter.com/camilohuinca">@camilohiunca</a>
+      </div>
+
+      <div class="col-md-8">
+        <p>
+          Diseñador gráfico de DuocUC (2012), especializado en ilustración, desarrollo de marca y dirección de arte. Su trabajo como artista es mas conocido bajo el nombre de OnlyJoke, ha sido publicado y mencionado por variados medios relacionados a la disciplina. Junto con su estilo ha logrado desarrollar proyectos con grandes marcas de nivel local e internacional.
+        </p>
+      </div>
+
+
+    </div>
+  </div>
+</section>
+
+<section class="container-fluid" id="francisco">
+  <div class="container">
+    <h1>Francisco</h1>
+  </div>
+</section>
+
+<section class="container-fliud" id="datos-francisco">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 redes text-right">
+        <h4>Instagram</h4>
+        <a href="http://www.Instagram.com/dmnc1">@dmnc1</a>
+        <br />  <br />
+        <h4>Twitter</h4>
+        <a href="http://twitter.com/dmnc1">@dmnc1</a>
+      </div>
+
+      <div class="col-md-8">
+        <p>
+        Diseñador gráfico de Arcos (2011), especializado en visualísmo, 3D y dirección de arte. Su trabajo como artista es mas conocido bajo el nombre de DMNC, ha tenido importantes participaciones en la producción y dirección de diversos liveshows en la escena musical local.
+        </p>
+      </div>
+    </div>
+  </div>
 </section>
 
 <!-- Archivo de barra lateral por defecto -->
